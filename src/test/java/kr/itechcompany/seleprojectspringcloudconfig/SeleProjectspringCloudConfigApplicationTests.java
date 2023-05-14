00670ec511a8935fae5982c77fc6f70de499a390
노종철 <eoeodlstkwlfhd@blusterbros.com>
2023-05-15 00:21:53 +0900

    bn

---
+++ src/test/java/kr/itechcompany/seleprojectspringcloudconfig/SeleProjectspringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.itechcompany.seleprojectspringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleProjectspringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
